4dcad1d513906c29412cc770690d0342dfb97f79
Jason Koenig <jrkoenig@stanford.edu>
2016-02-03 12:59:14 -0800

    Add summary code

---
+++ resultsummary.py
@@ -0,0 +1,41 @@
+import pg
+import json, math, sys, random
+
+def first_order_stats(data):
+    mean = sum([math.log(d) for d in data])/float(len(data))
+    sd = (sum([(math.log(d) - mean)**2 for d in data])/float(len(data)-1))**0.5
+    return math.exp(mean), math.exp(sd)
+
+def mean(l):
+    return sum(l)/float(len(l))
+def median(l):
+    a = sorted(l)
+    if len(a) % 2 == 1:
+        return a[len(a)/2]
+    else:
+        return (a[len(a)/2] + a[len(a)/2-1])*0.5
+
+def get_iter_counts(conn, series):
+    itercounts = {}
+    c = conn.cursor()
+    c.execute("SELECT result_json FROM distr_output WHERE series = %s",[series])
+    for (l,) in c:
+    #for l in open(series,"r"):
+        result = json.loads(str(l.strip()), 'utf-8')
+        n = result["name"]
+        i = int(result["iters"]) if result["correct"] else None
+        if n in itercounts:
+            itercounts[n].append(i)
+        else:
+            itercounts[n] = [i]
+    conn.commit()
+    return itercounts
+
+def main():
+    itercounts = {}
+    MAX = 20000000
+
+    conn = pg.connect('jrkoenig')
+    
+if __name__ == "__main__":
+    main()
--- submitseries.py
+++ submitseries.py
@@ -4,14 +4,15 @@ import threading, json, os, sys, gzip, random
 import pg, pgmq, targetbuilder
 
 
-RUNS = 1
-TIMEOUT = 10*1000*1000
-series_name = "test01"
+RUNS = 500
+TIMEOUT = 20*1000*1000
+series_name = "strict_nonincrease"
 progs = ['p{:02d}'.format(i+1) for i in range(25)]
 progs = [p for p in progs if p != 'p19']
 #progs = ['p18','p10','p11','p12','p17','p20','p21','p24']
+#progs = ['p19']
 randomize = False
-stoke_hash = '069352da94fa101afb8852be61ba4b9ff04e8cbf'
+stoke_hash = 'd2a59111d6d16590d93e7bb39f3bece2de0aabdf'
 
 def main():
     targets = targetbuilder.make_all_from_c("gulwani/gulwani.json")
@@ -25,7 +26,9 @@ def main():
     jobs = [{'series': series_id,
               'name': n, 'stoke_hash': stoke_hash, 'target': targets[n].to_json(),
               'timeout':TIMEOUT} for n in progs]
-    l = list(jobs * RUNS)
+    l = []
+    for j in jobs:
+        l.extend([j]*RUNS)
     if randomize:
         l = random.sample(l, len(l))
     jobs = [pg.Binary(json.dumps(j, separators=(',',':'), ensure_ascii=True)) for j in l]
