39e465425b6a61236ecf0b160c9f2d42286fa1ed
Jason Koenig <jrkoenig@stanford.edu>
2016-11-02 15:03:46 -0700

    Add fast family loader

--- new_synth.py
+++ new_synth.py
@@ -1,11 +1,12 @@
 
 import time, random, cPickle, json, synth
 from synth import stokerunner
+from synth.families import FamilyLoader
 
 def run_tasks(task_producer, max_tasks = 2):
     outstanding = []
-    have_more = True
     try:
+        have_more = True
         while have_more or len(outstanding) > 0:
             while have_more and len(outstanding) < max_tasks:
                 try:
@@ -20,47 +21,6 @@ def run_tasks(task_producer, max_tasks = 2):
         for o in outstanding:
             o.kill()
             
-class Target(object):
-    def __init__(self, di, lo, instrs):
-        self.def_in = di
-        self.live_out = lo
-        self.instrs = instrs
-    def __str__(self):
-        return "(" + ",".join(self.def_in) +")->("+",".join(self.live_out)+") {" + "; ".join(self.instrs) + "}"
-    def size(self): return len(instrs)
-
-class Family(object):
-    def __init__(self, target):
-        self.head = target
-        self.rest = []
-    def __str__(self):
-        return str(self.head) + " & " + ",".join(map(str,self.rest))
-    def merge_if_contained(self, target):
-        if self.head.def_in != target.def_in:
-            return False
-        s = len(self.head.instrs)
-        if s == len(target.instrs) and self.head.instrs == target.instrs:
-            # target is the same as head. ignore so we don't get duplicates
-            return True
-        elif s < len(target.instrs) and self.head.instrs == target.instrs[:s]:
-            self.rest.append((s,self.head.live_out))
-            self.head = target
-            return True
-        elif s > len(target.instrs) and target.instrs == self.head.instrs[:len(target.instrs)]:
-            self.rest.append((len(target.instrs), target.live_out))
-            self.rest.sort(key = lambda x: x[0])
-            return True
-        else:
-            return False
-
-
-def make_target(target):
-    t = synth.SynthTarget()
-    t.def_in = target.def_in
-    t.live_out = filter(lambda x: x != "%af", target.live_out)
-    t.target = ".f:\n" + "\n".join(target.instrs) +"\nretq\n"
-    t.testcases = ""
-    return t
 
 class StokeTask(object):
     def __init__(self, i, target, out):
@@ -72,7 +32,7 @@ class StokeTask(object):
         runner = self.runner
         runner.setup(self.target, None)
         runner.add_args(["--timeout_iterations", str(50000000)])
-        runner.add_args(["--timeout_seconds", str(900)])
+        runner.add_args(["--timeout_seconds", str(60)])
         runner.add_args(["--validator_must_support"])
         runner.add_args(["--generate_testcases"])
         runner.launch()
@@ -103,15 +63,23 @@ class StokeTask(object):
         self.output_file.flush()
         return True
 
+def make_target(target):
+    t = synth.SynthTarget()
+    t.def_in = target.def_in
+    t.live_out = filter(lambda x: x != "%af", target.live_out)
+    t.target = ".f:\n" + "\n".join(target.instrs) +"\nretq\n"
+    t.testcases = ""
+    return t
+
 def stoke_tasks():
-    families = cPickle.load(open("libs.families.pickle", "r"))
-    jobs = [i for i,f in enumerate(families)]
+    families = FamilyLoader("libs.families")
+    jobs = list(range(len(families)))
     random.shuffle(jobs)
     output_file = open("r.jsonl", "w")
     for i in jobs:
         yield StokeTask(i, make_target(families[i].head), output_file)
 
 def main():
-    run_tasks(stoke_tasks(), 28)
+    run_tasks(stoke_tasks(), 2)
 
 main()
---
+++ synth/families.py
@@ -0,0 +1,92 @@
+
+import json, struct
+
+class Target(object):
+    def __init__(self, di, lo, instrs):
+        self.def_in = di
+        self.live_out = lo
+        self.instrs = instrs
+    def __str__(self):
+        return "(" + ",".join(self.def_in) +")->("+",".join(self.live_out)+") {" + "; ".join(self.instrs) + "}"
+    def size(self): return len(self.instrs)
+
+class Family(object):
+    def __init__(self, target):
+        self.head = target
+        self.rest = []
+    def __str__(self):
+        return str(self.head) + " & " + ",".join(map(str,self.rest))
+    def merge_if_contained(self, target):
+        if self.head.def_in != target.def_in:
+            return False
+        s = len(self.head.instrs)
+        if s == len(target.instrs) and self.head.instrs == target.instrs:
+            # target is the same as head. ignore so we don't get duplicates
+            return True
+        elif s < len(target.instrs) and self.head.instrs == target.instrs[:s]:
+            self.rest.append((s,self.head.live_out))
+            self.head = target
+            return True
+        elif s > len(target.instrs) and target.instrs == self.head.instrs[:len(target.instrs)]:
+            self.rest.append((len(target.instrs), target.live_out))
+            self.rest.sort(key = lambda x: x[0])
+            return True
+        else:
+            return False
+
+class FamilyEncoder(json.JSONEncoder):
+     def default(self, obj):
+         if isinstance(obj, Family):
+             return {"head": self.default(obj.head),"rest":obj.rest}
+         elif isinstance(obj, Target):
+             return [obj.def_in, obj.live_out, obj.instrs]
+
+def save_families(families, fname):
+    f = open(fname, "w")
+    n = len(families)
+    positions = bytearray(4*(n+2))
+    f.write(positions)
+    struct.pack_into("=I",positions, 0, n)
+    assert f.tell() == 4*(n+2)
+    for i,fam in enumerate(families):
+        struct.pack_into("=I",positions, 4+4*i, f.tell())
+        json.dump(fam, f, cls=FamilyEncoder)
+    
+    struct.pack_into("=I",positions, 4+4*n, f.tell())
+    f.seek(0)
+    f.write(positions)
+    f.close()
+
+def _decoder(j):
+    h = j['head']
+    family = Family(Target(h[0], h[1], h[2]))
+    family.rest = [(x[0],x[1]) for x in j['rest']]
+    return family
+    
+class FamilyLoader(object):
+    def __init__(self, fname):
+        self.f = open(fname, "r")
+        size = self.f.read(4)
+        self.n = struct.unpack_from("=I", size, 0)[0]
+        self.positions = self.f.read(4*(self.n+1))
+    def __len__(self): return self.n
+    def __getitem__(self, i):
+        if not(0 <= i and i < self.n):
+            raise IndexError
+        offset = struct.unpack_from("=I", self.positions, 4*i)[0]
+        next_offset = struct.unpack_from("=I", self.positions, 4*i+4)[0]
+        self.f.seek(offset)
+        return json.loads(self.f.read(next_offset-offset), object_hook=_decoder)
+
+def _main():
+    import pickle
+    families = pickle.load(open("libs.families.pickle"))
+    save_families(families, "libs.families")
+    
+def _test():
+    loader = FamilyLoader("libs.families")
+    print "len is", len(loader)
+    c = 0
+    for i in loader:
+        c += i.head.size()
+    print "total size is", c
