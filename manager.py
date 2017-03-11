4b9c0a3f132ddad4ffc267686d2403ff5fd0cf6c
Jason Koenig <jrkoenig@stanford.edu>
2017-03-10 18:03:01 -0800

    Add distribution framework

---
+++ manager.py
@@ -0,0 +1,146 @@
+
+args = ["--timeout_iterations", "10000000","--timeout_seconds", str(60*60*24*2),"--validator_must_support","--generate_testcases"]
+targets = [229810,105886,197933,66217,227723,82322,147378]
+
+class JobSource(object):
+    def __init__(self):
+        self.l = [("{:06d}".format(i), {'target': targets[i%len(targets)], 'args': args}) for i in range(10000)]
+    def pop(self):
+        n = self.l[0]
+        self.l = self.l[1:]
+        return n
+    def __len__(self):
+        return len(self.l)
+
+import threading
+import status
+
+
+class JobManager(object):
+    def __init__(self, ):
+        self.servers_last_seen = {}
+        self.inflight = {}
+        self.notstarted = set()
+        self.completed = {}
+        self.source = JobSource()
+        self.lock = threading.Lock()
+    
+    def mark_alive(self, server):
+        with self.lock:
+            self.servers_last_seen[server] = time.time()
+    def get(self, s):
+        with self.lock:
+            if len(self.notstarted) > 0:
+                n = self.notstarted.items()[0]
+                self.notstarted.remove(n)
+                d = self.inflight[n][1]
+            elif len(self.source) > 0:
+                n, d = self.source.pop()
+            else:
+                return []
+            self.inflight[n] = (s, d)
+            return [(n,d)]
+    def complete(self, job, result):
+        with self.lock:
+            if job in self.inflight:
+                del self.inflight[job]
+                self.completed[job] = result
+                print "completed" , job
+                return True
+            else:
+                print "couldn't find job", job
+                return False
+    def cleanup(self):
+        with self.lock:
+            now = time.time()
+            timeout = 60
+            dead_servers = set([server for server, last_seen in self.servers_last_seen.items() if last_seen < now - timeout])
+            if len(dead_servers) > 0:
+                print "Lost servers", dead_servers
+                for job, (server, d) in self.inflight.items():
+                    if job not in self.notstarted and server in dead_servers:
+                        self.notstarted.add(job)
+                for server in dead_servers:
+                    del self.servers_last_seen[server]
+        
+jobs = JobManager()
+
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse, time, json
+
+def heartbeat(req, path, query):
+    jobs.mark_alive(query['server'][0])
+    req.send_response(200)
+
+def send_string(req, string):
+    req.send_response(200)
+    req.send_header('content-length', len(string))
+    req.end_headers()
+    req.wfile.write(string)
+
+def getjob(req, path, query):
+    rserver = query['server'][0]
+    jobs.mark_alive(rserver)
+    nextjobs = jobs.get(rserver)
+    send_string(req, json.dumps(nextjobs))
+
+def completejob(req, path, query):
+    server = query['server'][0]
+    job = query['job'][0]
+    jobs.mark_alive(server)
+    result = json.loads(req.rfile.read(int(req.headers.getheader('content-length'))))
+    if jobs.complete(job, result):
+        req.send_response(200)
+    else:
+        req.send_response(500)
+    
+def statusoverview(req, path, query):
+    send_string(req, status.overview(jobs))
+def statusjob(req, path, query):
+    job = query['job'][0]
+    send_string(req, status.job(jobs, job))
+def statuscompleted(req, path, query):
+    if 'job' in query:
+        r = status.result(jobs, query['job'][0])
+    else:
+        r = status.completed(jobs)
+    send_string(req, r)
+
+posthandlers = {'/heartbeat': heartbeat,
+                '/job/get': getjob,
+                '/job/complete': completejob}
+gethandlers = {'/status/overview': statusoverview,
+               '/': statusoverview,
+               '/status/completed': statuscompleted,
+               '/status/job': statusjob}
+
+class GetHandler(BaseHTTPRequestHandler):
+    def do(self, handlers):
+        parsed_path = urlparse.urlparse(self.path)
+        querydict = urlparse.parse_qs(parsed_path.query)
+        path = parsed_path.path
+        if path in handlers:
+            handlers[path](self, path, querydict)
+        else:
+            self.send_response(404)
+
+    def do_GET(self):
+        self.do(gethandlers)
+    def do_POST(self):
+        self.do(posthandlers)
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+def cleanup():
+    while True:
+        time.sleep(5)
+        jobs.cleanup()
+if __name__ == '__main__':
+    server = ThreadedHTTPServer(('localhost', 8080), GetHandler)
+    thread = threading.Thread(target = cleanup)
+    thread.start()
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
+    sys.exit(1)
---
+++ worker.py
@@ -0,0 +1,103 @@
+
+import httplib, time, random, json, urllib, socket
+import cPickle,synth, sys
+from synth import stokerunner
+from synth.families import FamilyLoader
+
+
+def make_target(target):
+    t = synth.SynthTarget()
+    t.def_in = target.def_in
+    t.live_out = filter(lambda x: x != "%af", target.live_out)
+    t.target = ".f:\n" + "\n".join(target.instrs) +"\nretq\n"
+    t.testcases = ""
+    return t
+
+class StokeTask(object):
+    def __init__(self, i, families, desc):
+        self.i = i
+        self.target = make_target(families[int(desc['target'])].head)
+        self.extra_args = desc.get('args', [])
+    def start(self):
+        self.runner = stokerunner.StokeRunner()
+        runner = self.runner
+        runner.setup(self.target, None)
+        runner.add_args(self.extra_args)
+        runner.launch()
+    def kill(self):
+        self.runner.proc.kill()
+    def finished(self):
+        if not self.runner.finished():
+            return False
+        runner = self.runner
+        j = runner.get_file("search.json")
+        if runner.successful():
+            print "STOKE finished on", self.i
+            r = json.loads(j)
+        else:
+            print "STOKE Failed on", self.i
+            print runner.get_file("stderr.out")
+            r = {"name":self.i,"error":True}
+        runner.cleanup()
+        conn.request("POST", "/job/complete?" + urllib.urlencode({'server': hostname, 'job': self.i}), json.dumps(r))
+        conn.getresponse()
+        return True
+
+def run_tasks(task_producer, max_tasks = 2):
+    outstanding = []
+    last_heartbeat = 0
+    try:
+        have_more = True
+        while have_more or len(outstanding) > 0:
+            while have_more and len(outstanding) < max_tasks:
+                try:
+                    t = next(task_producer)
+                    t.start()
+                    outstanding.append(t)
+                except StopIteration:
+                    have_more = False
+            time.sleep(0.1)
+            
+            outstanding = filter(lambda t: not t.finished(), outstanding)
+            now = time.time()
+            if now - last_heartbeat > 10:
+                conn.request("POST", "/heartbeat?server="+hostname)
+                resp = conn.getresponse()
+                last_heartbeat = now
+            else:
+                time.sleep(1)
+    except KeyboardInterrupt:
+        for o in outstanding:
+            o.kill()
+
+def stoke_tasks():
+    families = FamilyLoader("targets/libs.families")
+    while True:
+        conn.request("POST", "/job/get?server="+hostname)
+        resp = conn.getresponse()
+        if resp.status != 200:
+            print "Error from server"
+        else:
+            jobs = json.loads(resp.read())
+            if len(jobs) == 0:
+                time.sleep(random.uniform(10,20))
+            else:
+                (name, desc) = jobs[0]
+                yield StokeTask(name, families, desc)
+    
+def main():
+    global hostname, conn
+    conn = httplib.HTTPConnection("localhost:8080")
+    hostname = socket.gethostname()
+    if len(sys.argv) > 1:
+        conn = httplib.HTTPConnection(sys.argv[1])
+    else:
+        conn = httplib.HTTPConnection("localhost:8080")
+    if len(sys.argv) > 2:
+        threads = int(sys.argv[2])
+    else:
+        threads = 1
+    hostname += "-{:x}".format(random.randint(0,2**32))
+    run_tasks(stoke_tasks(), threads)
+
+main()
